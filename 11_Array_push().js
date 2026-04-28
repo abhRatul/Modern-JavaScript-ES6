93b7b97087d790a462da8baa9505f7fb8e12d35d
Abu Huraira Ratul <ratula9@gmail.com>
2026-04-28 11:29:13 +0600

    Array.push() method add

---
+++ 11_Array_push().js
@@ -0,0 +1,13 @@
+/* Notes
+- Array.push()
+- Main array ke change kore fele 
+- array er last e element add kore 
+- 1 ta 1 ta kore add kore
+- push() = element add করে + length return করে
+*/
+
+let number = [1,2,3,4,5];
+let res =  number.push(1,2,3,4,5);
+console.log(number);
+console.log(res); // 10
+
