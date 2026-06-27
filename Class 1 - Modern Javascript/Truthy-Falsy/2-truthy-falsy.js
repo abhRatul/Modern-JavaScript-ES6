53378ffd4894f4030d4617537d4089b11954569f
Ratul <ratula9@gmail.com>
2026-06-27 06:59:47 +0600

    feat: truthy falsy final source code

---
+++ Class 1 - Modern Javascript/Truthy-Falsy/2-truthy-falsy.js
@@ -0,0 +1,20 @@
+/*
+Truthy/Falsy values
+- Falsy values
+  - false, 0, "", NaN, null, undefined
+- Truthy values
+  - falsy value chara baki sob truthy value
+*/
+
+let value = 1;
+if(value){
+  console.log('Truthy');
+} else {
+  console.log('Falsy');
+}
+
+if(''){
+  console.log('Truthy');
+} else {
+  console.log('Falsy');
+}
